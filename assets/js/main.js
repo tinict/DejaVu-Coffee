bed50d7216a2a8759ea363ad2301c83c04b3d81b
Tín Nguyễn <89054327+tinict@users.noreply.github.com>
2022-08-04 17:36:14 +0700

    Test-01

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var menuElement = document.querySelector(".btn-menu");
+var contactElement = document.querySelector(".btn-contact");
+console.log(menuElement);
+console.log(contactElement);
+menuElement.onclick = function(){
+    console.log("Click");
+}
+contactElement.onclick = function(){
+    console.log("Click");
+}
